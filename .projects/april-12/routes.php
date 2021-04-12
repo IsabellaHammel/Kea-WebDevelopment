0ab80c748b3dbedcccc39bb551a30a4f1137d789
Isabella Hammel <bellahtj@hotmail.com>
2021-04-12 10:10:03 +0100

    added faker folder to seed users from db

--- .projects/april-12/routes.php
+++ .projects/april-12/routes.php
@@ -5,9 +5,40 @@ require_once(__DIR__.'/router.php'); // $_SERVER['DOCUMENT_ROOT'];
 // #####################
 
 get('/', function(){
-    echo 'x';
+    require_once($_SERVER['DOCUMENT_ROOT'].'/views/view_index.php'); // document root shouldnt be used in these folders
 });
 
+// #####################
+
+post('/users/create', function(){
+    echo 'user created';
+});
+
+// #####################
+
+post('/users/update/:id', function($id){
+    echo "updating user with id: $id";
+});
+
+// #####################
+
+post('/users/delete/:id', function($id){
+    echo "deleting user with id: $id";
+});
+
+// #####################
+
+post('/db-create-users', function(){
+    require_once($_SERVER['DOCUMENT_ROOT'].'/db/db_create_users.php');
+});
+
+// #####################
+
+post('/db-seed-users', function(){
+    require_once($_SERVER['DOCUMENT_ROOT'].'/db/db_seed_users.php');
+});
+
+// #####################
 
 any('/404', function(){
   echo 'Not found';
---
+++ .projects/april-12/views/view_index.php
@@ -0,0 +1,8 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/views/view_top.php');
+?>
+    Welcome
+
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/views/view_bottom.php');
+?>
