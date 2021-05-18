607275f1dd4a5e4957ff6eb23f773fd1aa1d51b2
Isabella Hammel <bellahtj@hotmail.com>
2021-05-18 17:45:15 +0100

    added cache service

--- services/cache_service.php
+++ services/cache_service.php
@@ -1,1 +1,10 @@
 <?php
+
+// https://www.tutorialspoint.com/redis/redis_php.htm
+
+class CacheService
+{
+    // IMPLEMENT
+    // cacheValue(key, string json) function
+    // getCachedValue(key) function
+}
